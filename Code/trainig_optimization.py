54ebe8856463fd2e7a6789285855ba6ca7b90110
nestor <nestor.calvo@udea.edu.co>
2024-04-22 07:46:21 -0500

    Results, graph and tables added

--- Code/trainig_optimization.py
+++ Code/trainig_optimization.py
@@ -25,7 +25,7 @@ IDs_path = './Metadata/IDs_Balanced.csv'
 tasks = ['vowel_a','vowel_i','vowel_u','phrase'] #'phrase' 'vowel_a'
 models = ['DT','RF'] #'DT' or 'RF' or 'XGBoost' or ['DT', 'RF']
 signal_type =  ['speech', 'egg']# or just 'egg' or 'speech'
-feature=['articulation', 'phonation'] #'BFCC' or 'GFCC' or 'non_linear' or 'all'
+feature=['MFCC', 'BFCC','GFCC', 'non_linear'] #'BFCC' or 'GFCC' or 'non_linear' or 'all'
 
 def models_optimization(folds_path, IDs_path, results_path, tasks, models, signal_type, feature):
     if feature == 'all':
